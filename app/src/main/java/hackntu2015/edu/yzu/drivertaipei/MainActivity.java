a87e0b6743fdaed5e9543352b4426a581ee2b6db
吳冠賢 <wuguanxian@108-20.wlan.yzu.edu.tw>
2015-08-15 13:00:50 +0800

    還不能定位

--- app/src/main/java/hackntu2015/edu/yzu/drivertaipei/MainActivity.java
+++ app/src/main/java/hackntu2015/edu/yzu/drivertaipei/MainActivity.java
@@ -7,22 +7,55 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import android.graphics.drawable.LevelListDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.Context;
 
 public class MainActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
+    LocationManager locationManager;
+
+    LocationListener locationListener = new LocationListener(){
+        @Override
+        public void onLocationChanged(Location location){
+            //implementation
+
+            setUpMap(location);
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider,int status,Bundle extras){}
+
+        @Override
+        public void onProviderEnabled(String provider){}
+        @Override
+        public void onProviderDisabled(String provider){}
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setUpMapIfNeeded();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        //setUpMapIfNeeded();
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+        //setUpMapIfNeeded();
     }
 
     /**
@@ -40,18 +73,6 @@ public class MainActivity extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
@@ -59,7 +80,15 @@ public class MainActivity extends FragmentActivity {
      * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    private void setUpMap(Location location) {
+
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(lat,lng))
+                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.gas_petrol_station_md)));
     }
+
+
 }
