260aba55c70e5332eaff18062dee0580ae851987
Andy <singbird81@gmail.com>
2015-08-15 13:36:55 +0800

    location self

--- app/src/main/java/hackntu2015/edu/yzu/drivertaipei/MainActivity.java
+++ app/src/main/java/hackntu2015/edu/yzu/drivertaipei/MainActivity.java
@@ -1,32 +1,20 @@
 package hackntu2015.edu.yzu.drivertaipei;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import android.graphics.drawable.LevelListDrawable;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.Bitmap;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-import java.util.List;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import hackntu2015.edu.yzu.drivertaipei.Node.NodeCarFlow;
-import hackntu2015.edu.yzu.drivertaipei.Node.NodeConstruct;
-import hackntu2015.edu.yzu.drivertaipei.Node.NodeGas;
-import hackntu2015.edu.yzu.drivertaipei.Node.NodeParkingLot;
-import hackntu2015.edu.yzu.drivertaipei.Node.NodeTraffic;
 import hackntu2015.edu.yzu.drivertaipei.controller.DataController;
-import hackntu2015.edu.yzu.drivertaipei.controller.DataListener;
-import hackntu2015.edu.yzu.drivertaipei.controller.DataManager;
-import hackntu2015.edu.yzu.drivertaipei.utils.ErrorCode;
 
 public class MainActivity extends FragmentActivity {
 
@@ -35,50 +23,36 @@ public class MainActivity extends FragmentActivity {
 
     LocationManager locationManager;
 
-    LocationListener locationListener = new LocationListener(){
-        @Override
-        public void onLocationChanged(Location location){
-            //implementation
-
-            setUpMap(location);
-
-
-        }
-
-        @Override
-        public void onStatusChanged(String provider,int status,Bundle extras){}
-
-        @Override
-        public void onProviderEnabled(String provider){}
-        @Override
-        public void onProviderDisabled(String provider){}
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-<<<<<<< HEAD
-        setUpMapIfNeeded();
-        DataManager.getInstance().setListener(new DataListener() {
+        //setUpMapIfNeeded();
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        LocationListener locationListener = new LocationListener(){
             @Override
-            public void onDataUpdate(List<NodeCarFlow> carFlowList, List<NodeParkingLot> parkingLotList, List<NodeTraffic> trafficList, List<NodeConstruct> constructsList, List<NodeGas> nodeGas) {
-                setUpMapIfNeeded();
+            public void onLocationChanged(Location location){
+                //implementation
+                Log.e("XXX","XXX"+location);
+                setUpMap(location);
+
+
             }
 
             @Override
-            public void onDataConnectFailed(ErrorCode err) {
+            public void onStatusChanged(String provider,int status,Bundle extras){}
 
-            }
-        });
-        DataManager.getInstance().updateData();
-=======
+            @Override
+            public void onProviderEnabled(String provider){}
+            @Override
+            public void onProviderDisabled(String provider){}
+        };
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-        //setUpMapIfNeeded();
-
->>>>>>> a87e0b6743fdaed5e9543352b4426a581ee2b6db
+        mMap.setMyLocationEnabled(true);
     }
 
     @Override
@@ -109,11 +83,12 @@ public class MainActivity extends FragmentActivity {
      * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-<<<<<<< HEAD
+
     private void setUpMap() {
 
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-=======
+    }
+
     private void setUpMap(Location location) {
 
         double lat = location.getLatitude();
@@ -122,7 +97,7 @@ public class MainActivity extends FragmentActivity {
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(lat,lng))
                         .icon(BitmapDescriptorFactory.fromResource(R.mipmap.gas_petrol_station_md)));
->>>>>>> a87e0b6743fdaed5e9543352b4426a581ee2b6db
+
     }
 
 
