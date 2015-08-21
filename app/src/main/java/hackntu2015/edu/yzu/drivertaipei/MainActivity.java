bc038587c4731c1fa0b6201a3a6c607ac44a9bc8
Andy <singbird81@gmail.com>
2015-08-22 02:53:33 +0800

    optimize alogorithm

--- app/src/main/java/hackntu2015/edu/yzu/drivertaipei/MainActivity.java
+++ app/src/main/java/hackntu2015/edu/yzu/drivertaipei/MainActivity.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
